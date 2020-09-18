4b9a1084c5663a6a965939dd07aeef763c77963f
simonyjs <cage@live.co.kr>
2020-09-18 14:40:57 +0900

    Node.js-37.App 제작-글삭제-삭제버튼 구현

--- main.js
+++ main.js
@@ -62,7 +62,12 @@ var app = http.createServer(function (request, response) {
             title,
             list,
             `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            ` <a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>`
           );
           response.writeHead(200);
           response.end(template);
@@ -147,6 +152,19 @@ var app = http.createServer(function (request, response) {
           response.writeHead(302, { Location: `/?id=${title}` });
           response.end();
         });
+      });
+    });
+  } else if (pathname === "/delete_process") {
+    var body = "";
+    request.on("data", function (data) {
+      body = body + data;
+    });
+    request.on("end", function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function (error) {
+        response.writeHead(302, { Location: `/` });
+        response.end();
       });
     });
   } else {
